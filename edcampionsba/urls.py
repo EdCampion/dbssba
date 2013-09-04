6ba794b5e7f872c2bfe1febbeee3f94e8ef62dcd
Ed <edcampion@gmail.com>
2013-09-04 20:09:36 +0100

    Dialed back models, got streaming working in basic fashion. Now for aws test

--- edcampionsba/urls.py
+++ edcampionsba/urls.py
@@ -7,11 +7,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'edcampionsba.views.home', name='home'),
+     url(r'^sba/', include('sba.urls')),
     # url(r'^edcampionsba/', include('edcampionsba.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    
      url(r'^admin/', include(admin.site.urls)),
 )
--- sba/views.py
+++ sba/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.http import HttpResponse
+import TwitterTool
+def index(request):
+    TwitterTool.Run()
+    return HttpResponse("Hello, world. You're at the tweetz index.")# Create your views here.# Create your views here.
