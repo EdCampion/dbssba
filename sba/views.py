ad7e3353f9d106ad4710112324ef7df8e6482f51
Ed <Ed@10.69.65.93>
2013-09-19 11:27:36 +0100

    Added success message

--- sba/views.py
+++ sba/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 import TwitterTool
 def index(request):
     TwitterTool.Run()
-    return HttpResponse("Hello, world. You're at the tweetz index.")# Create your views here.# Create your views here.
+    return HttpResponse("Tweet Collection Successfully Completed!")
